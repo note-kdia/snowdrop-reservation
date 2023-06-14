0ba0590e1f3bc0636f87fd5c9632b64f9cea229e
note-kdia <naruse147@gmail.com>
2023-06-15 00:34:40 +0900

    ✨ webcam capture

--- react/reception/src/App.tsx
+++ react/reception/src/App.tsx
@@ -1,8 +1,51 @@
-import './App.css';
+import { useRef, useState } from "react"
+import Webcam from "react-webcam"
+import './App.css'
+
+const videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: "user",
+}
 
 export default function Index() {
   return (
-    <h1>Index View</h1>
+    <>
+      <h1>Index View</h1>
+      <CameraView />
+    </>
   )
 }
 
+function CameraView() {
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false)
+  const webcamRef = useRef<Webcam>(null)
+
+  return (
+    <>
+      <header>
+        <h3>Camera View</h3>
+      </header>
+      {isCaptureEnable || (
+        <button onClick={() => setCaptureEnable(true)}>Start</button>
+      )}
+      {isCaptureEnable && (
+        <>
+          <div>
+            <button onClick={() => setCaptureEnable(false)}>Exit</button>
+          </div>
+          <div>
+            <Webcam
+              audio={false}
+              width={540}
+              height={360}
+              ref={webcamRef}
+              screenshotFormat='image/jpeg'
+              videoConstraints={videoConstraints}
+            />
+          </div>
+        </>
+      )}
+    </>
+  )
+}
